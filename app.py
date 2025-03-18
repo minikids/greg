95e10ed53b1cfd1cd034e0f7cc0a19f1e3cebe56
minikids <96962258+minikids@users.noreply.github.com>
2025-03-18 11:19:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ def home():
     return render_template('index.html', greeting=greeting_message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
